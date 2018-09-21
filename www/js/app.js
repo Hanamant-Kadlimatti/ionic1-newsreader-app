665fb4abb136800df0eaac1d30bcda7fc3af92b5
Hanamant Ramappa Kadlimatti <hanamantramap_k@photonint.com>
2018-09-21 19:40:08 +0530

    first commit

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,25 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+
+app.controller('newsController', function($scope, $http){
+
+  $scope.news = []
+
+     $http({
+       method: "GET",
+       url :  "https://jsonplaceholder.typicode.com/photos"
+     }).then(function (newsData){
+      
+      //  console.log(newsData)
+       $scope.news = newsData.data
+       console.log($scope.news)
+     })
+})
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs).
